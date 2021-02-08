c99d1829226272d3d87b37da325b6bf35bae711d
David Woodward <davidiwoodward@icloud.com>
2021-02-08 12:11:04 -0500

    yeet

--- index.ts
+++ index.ts
@@ -7,6 +7,7 @@ server.get("/", async (req, reply) => {
 });
 
 const PORT = process.env.PORT || 5000;
+console.log("this is port", process.env.PORT, "this is what I got", PORT);
 server.listen(PORT, (err, addr) => {
   if (err) {
     console.error(err);

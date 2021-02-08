4f8e2882f5c6d3efd4ca8081129d20d26a6bb5e1
David Woodward <davidiwoodward@icloud.com>
2021-02-08 11:59:04 -0500

    yay

--- index.ts
+++ index.ts
@@ -1,16 +1,17 @@
-import fastify from 'fastify'
+import fastify from "fastify";
 
 const server = fastify();
 
-server.get('/ping', async (req, reply) => {
-  return 'pong\n';
+server.get("/", async (req, reply) => {
+  return "pong\n";
 });
 
-server.listen(8000, (err, addr) => {
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, (err, addr) => {
   if (err) {
     console.error(err);
     process.exit(1);
   }
 
-  console.log(`Listening on ${addr}`);
+  console.log("ready");
 });
